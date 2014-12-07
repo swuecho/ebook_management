1edc25cde2effcb5fc21f7f7442f4e1f00e8b752
Hao Wu <hwu@intellisurvey.com>
2014-12-06 17:29:31 -0800

    image

--- display_all_upload.pl
+++ display_all_upload.pl
@@ -3,19 +3,51 @@ use v5.20;
 use lib '.';
 use Me::Schema;
 use Dancer2;
+use Imager;
+use DDP;
 
 my $schema = Me::Schema->connect('dbi:SQLite:Me.db');
 
+get '/index' => sub {
 
-get '/*' => sub {
-# retrieve the upload
-my $upload = $schema->resultset('Upload')->find(1);
+    # retrieve the upload
+    my @all_uploads = $schema->resultset('Upload')->all();
 
-# get the absolute path
-say $upload->file->absolute;
-say $upload->ts;
-say $upload->name;
-return $upload->name;
+    @all_uploads =
+      map { $_->{thumbnail_url} = uri_for( '/photo/thumbnail/' . $_->{id} ); $_ }
+      grep { $_->{mime} =~m/image/ } 
+      map { $_->{_column_data} } @all_uploads;
+
+    template 'index.tt', { all_uploads => \@all_uploads };
+
+};
+
+get '/photo/thumbnail/:id' => sub {
+
+    my $photo = $schema->resultset('Upload')->find( { id => params->{id} } );
+
+    # why this not work?
+    #    p $photo->extension;
+
+    my $file = $photo->file;
+    my ($ext) = mime->for_file( $photo->name )->extensions;
+
+    my $data = $file->open('r') or die "Error: $!";
+
+    my $img = Imager->new;
+    $img->read( fh => $data ) or die $img->errstr;
+    my $scaled = $img->scale( xpixels=> 200, ypixels => 200 );
+
+    my $out;
+    $scaled->write(
+        type => $ext,
+        data => \$out
+    ) or die $scaled->errstr;
+    content_type( $photo->mime );
+    headers( "Content-Disposition" => "inline", "Content-Length" => -s $out );
+    #binmode $out; # when use this?
+    content($out);
+
 };
 
 dance;
